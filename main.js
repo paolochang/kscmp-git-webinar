516e9bf8b2fc0952f7580f07cab0c9a6d25eefcc
Paolo Chang <chanhwan.chang@gmail.com>
2021-10-29 19:35:50 -0400

    Add getAge function

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-import { add, subtract } from "./calculator/calculator.js";
+import { add, subtract, getAge } from "./calculator/calculator.js";
 
 console.log("Hello, Git");
 
 console.log(add(3, 6));
 console.log(subtract(6, 2));
+
+let name = "Pororo";
+let intro = `Hello, I'm ${name} and I'm ${getAge(2003)} years old`;
+
+console.log(intro);
